308498e4727544753a568a9a98875e37175f8d0e
vivien <vlad-vivien@orange.fr>
2024-04-06 00:31:39 +0200

    maj os

--- AppCsharp/obj/Debug/net8.0/AppCsharp.AssemblyInfo.cs
+++ AppCsharp/obj/Debug/net8.0/AppCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00628da962251830b34f0d75c50e1fde9055b6c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7fc8204101dad391d55543d6f67efd6fe4595e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
